f95902daa331d444588d097cf38c314e77219d50
LoylP <nhphuc183@gmail.com>
2024-10-08 14:03:47 +0700

    add file requirements

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from routes.entry import entry_root
 from routes.data import data_root
+import uvicorn
 
 app = FastAPI()
 
-
 app.include_router(entry_root)
 app.include_router(data_root)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
